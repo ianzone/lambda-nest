45f3afb15193abf1fadd657fd2c89068d4411d51
ian <yihengz@outlook.com>
2024-03-22 19:21:55 +0800

    feat: 🎸 validation pipe

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { makeDocs } from 'src/utils';
 import { AppModule } from './app.module';
 
 export async function createApp() {
-  const isDev = process.env.NODE_ENV !== 'dev';
+  const isDev = process.env.NODE_ENV === 'dev';
   const app = await NestFactory.create(AppModule, {
     logger: isDev ? undefined : console,
   });
+  app.useGlobalPipes(
+    // https://docs.nestjs.com/techniques/validation#using-the-built-in-validationpipe
+    new ValidationPipe({
+      whitelist: true, // strips away any properties that do not have any decorators in DTO
+      forbidNonWhitelisted: true, // if a property is not in the DTO, throw an error
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+        exposeUnsetFields: false, // remove undefined properties
+      },
+    }),
+  );
   makeDocs(app);
   return app;
 }
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import { createApp } from './app';
 async function bootstrap() {
   const app = await createApp();
   await app.listen(3000);
+  console.log('Swagger UI available at http://localhost:3000/docs');
 }
 bootstrap();
